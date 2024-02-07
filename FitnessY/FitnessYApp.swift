47d2c808823f16e0dc50824579a5ceb839139409
Albin Sander <51294735+Albin-Sander@users.noreply.github.com>
2024-02-07 21:04:41 +0100

    Initial Commit

---
+++ FitnessY/FitnessYApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessYApp.swift
+//  FitnessY
+//
+//  Created by Albin Sander on 2024-02-07.
+//
+
+import SwiftUI
+
+@main
+struct FitnessYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
